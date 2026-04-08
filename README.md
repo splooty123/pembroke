1bd68c3de441ba836ea6ea6d1981f67f1b7b3c32
splooty123 <splooty123@outlook.com>
2026-04-07 22:21:26 -0700

    added stuff

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# Pembroke
+Pembroke is a single file C library for creating and animating math videos.
+# Dependancies
--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main() {
     for (int i = 0; i < 300; i++) {
         clear_frame(255);
         graph_rpn("x x * 5000 * t * 497.5 -", 4, (color) { 0, 0, 0 });
-        write_latex("x^2", 20, 20, (color) { 255, 0, 0 });
+        write_latex("x^2", 20, 20, (color) { 0, 0, 0 });
         save_frame();
         progress(300);
     }
